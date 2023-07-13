bb3cf296cec13d5dde69b3bd93f6e93b7c044b1b
Sourav Sarkar <souravsantipur9635@gmail.com>
2023-07-13 13:30:21 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 
-const connection = mongoose.connect(process.env.mongoURL)
+const connection = mongoose.connect(`mongodb+srv://sohel:nopass@cluster0.4rmgup6.mongodb.net/LoginSignUpEval?retryWrites=true&w=majority`)
 
 module.exports={
     connection

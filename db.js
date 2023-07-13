cdd6c768ddf6918d3df28151f5740047eb367f0b
Sourav Sarkar <souravsantipur9635@gmail.com>
2023-07-13 15:33:32 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 
-const connection = mongoose.connect(`mongodb+srv://sohel:nopass@cluster0.4rmgup6.mongodb.net/LoginSignUpEval?retryWrites=true&w=majority`)
+const connection = mongoose.connect(`mongodb+srv://sourav:sourav@cluster0.10rxc0r.mongodb.net/u7evalemployee?retryWrites=true&w=majority`)
 
 module.exports={
     connection
